22dc5648cd61ba71bc8b120a2e530cdc137f25ad
Jaymin Mistry <92642106+jaymin-codes@users.noreply.github.com>
2025-09-08 22:36:00 +0530

    Configure Astro with Tailwind, Icon, and Compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), icon(), compress()],
+  output: "server",
+  adapter: vercel()
+});
