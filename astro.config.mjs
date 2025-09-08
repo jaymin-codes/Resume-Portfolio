4984367f70a3f28da9200b86dd6934a8e2c5b4ea
Jaymin Mistry <92642106+jaymin-codes@users.noreply.github.com>
2025-09-08 22:44:12 +0530

    Change output type from 'server' to 'static'

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
-  output: "server",
+  output: "static",
   adapter: vercel()
 });
